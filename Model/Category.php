cf6cae432028e0c45d0591b860b7fe584a71dd00
Steve Kukla <stevenjkukla@gmail.com>
2018-06-29 19:25:17 -0400

    Initial commit

---
+++ Model/Category.php
@@ -0,0 +1,240 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Kukla\SampleDataBuilder\Model;
+
+use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
+
+/**
+ * Class Category
+ */
+class Category
+{
+    /**
+     * @var \Magento\Framework\Setup\SampleData\FixtureManager
+     */
+    protected $fixtureManager;
+
+    /**
+     * @var \Magento\Catalog\Api\Data\CategoryInterfaceFactory
+     */
+    protected $categoryFactory;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Category\TreeFactory
+     */
+    protected $resourceCategoryTreeFactory;
+
+
+    /**
+     * @var \Magento\Framework\Data\Tree\Node
+     */
+    protected $categoryTree;
+
+    /**
+     * @var \Magento\Store\Api\Data\StoreInterfaceFactory
+     */
+    protected $storeFactory;
+
+    /**
+     * @var \Magento\Cms\Api\Data\BlockInterfaceFactory
+     */
+    protected $blockFactory;
+
+    /**
+     * @param SampleDataContext $sampleDataContext
+     * @param \Magento\Catalog\Api\Data\CategoryInterfaceFactory $categoryFactory
+     * @param \Magento\Catalog\Model\ResourceModel\Category\TreeFactory $resourceCategoryTreeFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Store\Api\Data\StoreInterfaceFactory $storeFactory
+     * @param \Magento\Cms\Api\Data\BlockInterfaceFactory $blockFactory
+
+     */
+
+
+    public function __construct(
+        SampleDataContext $sampleDataContext,
+        \Magento\Catalog\Api\Data\CategoryInterfaceFactory $categoryFactory,
+        \Magento\Catalog\Model\ResourceModel\Category\TreeFactory $resourceCategoryTreeFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Store\Api\Data\StoreInterfaceFactory $storeFactory,
+        \Magento\Cms\Api\Data\BlockInterfaceFactory $blockFactory
+    ) {
+        $this->fixtureManager = $sampleDataContext->getFixtureManager();
+        $this->csvReader = $sampleDataContext->getCsvReader();
+        $this->categoryFactory = $categoryFactory;
+        $this->resourceCategoryTreeFactory = $resourceCategoryTreeFactory;
+        $this->storeManager = $storeManager;
+        $this->storeFactory = $storeFactory;
+        $this->blockFactory = $blockFactory;
+    }
+
+    /**
+     * @param array $fixtures
+     * @throws \Exception
+     */
+    public function install(array $fixtures)
+    {
+        foreach ($fixtures as $fileName) {
+            $fileName = $this->fixtureManager->getFixture($fileName);
+            if (!file_exists($fileName)) {
+                continue;
+            }
+            $rows = $this->csvReader->getData($fileName);
+            $header = array_shift($rows);
+            foreach ($rows as $row) {
+                $data = [];
+                foreach ($row as $key => $value) {
+                    $data[$header[$key]] = $value;
+                }
+                $this->createCategory($data);
+            }
+        }
+    }
+
+    /**
+     * @param array $row
+     * @param \Magento\Catalog\Model\Category $category
+     * @return void
+     */
+    protected function setAdditionalData($row, $category)
+    {
+        $additionalAttributes = [
+            'position',
+            'display_mode',
+            'page_layout',
+            'custom_layout_update',
+            'look_book_main_image',
+            'description',
+            'landing_page'
+        ];
+
+        foreach ($additionalAttributes as $categoryAttribute) {
+            if (!empty($row[$categoryAttribute])) {
+                if($categoryAttribute == 'landing_page'){
+                    $attributeData = [$categoryAttribute => $this->getCmsBlockId($row[$categoryAttribute])];
+                }else {
+                    $attributeData = [$categoryAttribute => $row[$categoryAttribute]];
+                }
+                $category->addData($attributeData);
+            }
+        }
+    }
+
+    /**
+     * Get category name by path
+     *
+     * @param string $path
+     * @return \Magento\Framework\Data\Tree\Node
+     */
+    protected function getCategoryByPath($path)
+    {
+        $store = $this->storeFactory->create();
+        $store->load('venia_us');
+        $rootCatId = $store->getGroup()->getDefaultStore()->getRootCategoryId();
+        $names = array_filter(explode('/', $path));
+        $tree = $this->getTree($rootCatId);
+        foreach ($names as $name) {
+            $tree = $this->findTreeChild($tree, $name);
+            if (!$tree) {
+                $tree = $this->findTreeChild($this->getTree($rootCatId, true), $name);
+            }
+            if (!$tree) {
+                break;
+            }
+        }
+        return $tree;
+    }
+
+    /**
+     * Get child categories
+     *
+     * @param \Magento\Framework\Data\Tree\Node $tree
+     * @param string $name
+     * @return mixed
+     */
+    protected function findTreeChild($tree, $name)
+    {
+        $foundChild = null;
+        if ($name) {
+            foreach ($tree->getChildren() as $child) {
+                if ($child->getName() == $name) {
+                    $foundChild = $child;
+                    break;
+                }
+            }
+        }
+        return $foundChild;
+    }
+
+    /**
+     * Get category tree
+     *
+     * @param int|null $rootNode
+     * @param bool $reload
+     * @return \Magento\Framework\Data\Tree\Node
+     */
+    protected function getTree($rootNode = null, $reload = false)
+    {
+        if (!$this->categoryTree || $reload) {
+            if ($rootNode === null) {
+                $rootNode = $this->storeManager->getDefaultStoreView()->getRootCategoryId();
+            }
+
+            $tree = $this->resourceCategoryTreeFactory->create();
+            $node = $tree->loadNode($rootNode)->loadChildren();
+
+            $tree->addCollectionData(null, false, $rootNode);
+
+            $this->categoryTree = $node;
+        }
+        return $this->categoryTree;
+    }
+
+    /**
+     * @param array $row
+     * @return void
+     */
+    protected function createCategory($row)
+    {
+        $category = $this->getCategoryByPath($row['path'] . '/' . $row['name']);
+        if (!$category) {
+            $parentCategory = $this->getCategoryByPath($row['path']);
+            $data = [
+                'parent_id' => $parentCategory->getId(),
+                'name' => $row['name'],
+                'is_active' => $row['active'],
+                'is_anchor' => $row['is_anchor'],
+                'include_in_menu' => $row['include_in_menu'],
+                'url_key' => $row['url_key'],
+                'store_id' => 0
+            ];
+            $category = $this->categoryFactory->create();
+            $category->setData($data)
+                ->setPath($parentCategory->getData('path'))
+                ->setAttributeSetId($category->getDefaultAttributeSetId());
+            $this->setAdditionalData($row, $category);
+
+            $category->save();
+
+        }
+    }
+    /**
+     * @param string $blockName
+     * @return int
+     */
+    protected function getCmsBlockId($blockName)
+    {
+        $block = $this->blockFactory->create();
+        $block->load($blockName, 'identifier');
+        return $block->getId();
+
+    }
+}
---
+++ Setup/Installer.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Kukla\SampleDataBuilder\Setup;
+
+use Magento\Framework\Setup;
+
+class Installer implements Setup\SampleData\InstallerInterface
+{
+    /**
+     * Setup class for category
+     *
+     * @var \Kukla\SampleDataBuilder\Model\Category
+     */
+    protected $categorySetup;
+
+    /**
+     * Setup class for product attributes
+     *
+     * @var \Kukla\SampleDataBuilder\Model\Attribute
+     */
+    protected $attributeSetup;
+
+    /**
+     * @param \MagentoEse\VeniaCatalogSampleData\Model\Category $categorySetup
+     * @param \MagentoEse\VeniaCatalogSampleData\Model\Attribute $attributeSetup
+     * @param \Magento\Framework\App\State $state
+     * @param \Magento\Indexer\Model\Processor $index
+     */
+
+    public function __construct(
+        \Kukla\SampleDataBuilder\Model\Category $categorySetup,
+        \Kukla\SampleDataBuilder\Model\Attribute $attributeSetup,
+        \Magento\Framework\App\State $state,
+        \Magento\Indexer\Model\Processor $index
+    ) {
+        $this->categorySetup = $categorySetup;
+        $this->attributeSetup = $attributeSetup;
+        $this->index = $index;
+        try{
+            $state->setAreaCode('adminhtml');
+        }
+        catch(\Magento\Framework\Exception\LocalizedException $e){
+            // left empty
+        }
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install()
+    {
+        // add attributes
+        $this->attributeSetup->install(['Kukla_SampleDataBuilder::fixtures/attributes.csv']);
+
+        // add categories
+        $this->categorySetup->install(['Kukla_SampleDataBuilder::fixtures/categories.csv']);
+
+        //reIndex as MECE redeploy will not automatically reindex
+        $this->index->reindexAll();
+    }
+}
